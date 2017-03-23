ab5186d6231652d50307cf48c13e22a86a41c688
Max Reuter <max.a.reuter@gmail.com>
2017-03-23 11:42:25 +0200

    turn generator into int

--- android/app/src/main/java/fi/aalto/mss/enhancedkeystore/DHGenParameters.java
+++ android/app/src/main/java/fi/aalto/mss/enhancedkeystore/DHGenParameters.java
@@ -4,6 +4,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.io.Serializable;
+import java.nio.ByteBuffer;
 
 /**
  * Created by max on 22.03.17.
@@ -11,23 +12,34 @@ import java.io.Serializable;
 
 public class DHGenParameters implements Parcelable, Serializable{
 
-    private byte[] generator;
+    private int generator;
     private byte[] prime;
     private byte[] publicKey;
 
     public DHGenParameters(byte[] generator, byte[] prime, byte[] publicKey) {
+        this.generator = convertByteArrayToInt(generator);
+        this.prime = prime;
+        this.publicKey = publicKey;
+    }
+
+    public DHGenParameters(int generator, byte[] prime, byte[] publicKey) {
         this.generator = generator;
         this.prime = prime;
         this.publicKey = publicKey;
     }
 
     protected DHGenParameters(Parcel in) {
-        generator = in.createByteArray();
+        generator = in.readInt();
         prime = in.createByteArray();
         publicKey = in.createByteArray();
     }
 
-    public byte[] getGenerator() {
+    private int convertByteArrayToInt(byte[] buffer) {
+        ByteBuffer wrapped = ByteBuffer.wrap(buffer); // big-endian by default
+        return wrapped.getInt();
+    }
+
+    public int getGenerator() {
         return generator;
     }
 
@@ -60,7 +72,7 @@ public class DHGenParameters implements Parcelable, Serializable{
     @Override
     public void writeToParcel(Parcel dest, int flags) {
 
-        dest.writeByteArray(generator);
+        dest.writeInt(generator);
         dest.writeByteArray(prime);
         dest.writeByteArray(publicKey);
     }

574a201e759a6c0fe41c135a9a040771c9195a04
antenehd <anteneh_assen@yahoo.com>
2017-03-30 15:26:32 +0300

    clear

--- android/otclient/src/main/java/fi/aalto/ssg/opentee/exception/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the exceptions used by the client library.
- */
-package fi.aalto.ssg.opentee.exception;
--- src/test/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-APP_ABI := armeabi-v7a
-APP_PIE := true
